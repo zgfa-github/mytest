6215bc6ae82dadb22fbf0c3f71d62389f18daaa7
zhuguangfu <zhuguangfu@qq.com>
2018-03-16 17:43:33 +0800

    2018-3-16

--- public/static/factory/js/uploadMultiImg.js
+++ public/static/factory/js/uploadMultiImg.js
@@ -1,5 +1,33 @@
 $(function(){
-
+        $('body').on('change','.uploadSingleImg',function(){
+            var img = event.target.files[0];
+            var obj=$(this).parent();
+            // 判断是否图片
+            if(!img){
+                return false;
+            }
+            // 判断图片格式
+            var imgRegExp=/\.(?:jpg|jpeg|png|gif)$/;
+            if(!(img.type.indexOf('image')==0 && img.type && imgRegExp.test(img.name)) ){
+                layer.open({
+                    content:'请上传：jpg、jpeg、png、gif格式图片',
+                    time:2
+                }) ;
+            }
+            var reader = new FileReader();
+            reader.readAsDataURL(img);
+            reader.onload = function(e){
+                var imgUrl=e.target.result;
+                $(obj).addClass('active');
+                var postData = {img: e.target.result};
+                postData.imgWidth = 145;
+                postData.imgHeight = 100; 
+                $(obj).find('img').attr('src',imgUrl);  
+                $.post('',abc,function(){
+                    
+                })           
+            }
+        });
         $('body').on('change','#file',function(){
             var file = $(this);
             var fileList = $(this).get(0).files;
